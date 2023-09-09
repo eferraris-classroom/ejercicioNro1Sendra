f1ced054380e67d73862b23deb361324ff42a1af
“julioleonel” <jsendra@fie.undef.edu.ar>
2023-09-09 18:46:22 -0300

    resolucion y comentarios

--- src/main/java/ar/edu/undef/fie/Main.java
+++ src/main/java/ar/edu/undef/fie/Main.java
@@ -4,6 +4,24 @@ package ar.edu.undef.fie;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length == 0) {
+            System.out.println("No se proporcionaron números para sumar.");
+            return;
+        }
+
+        // args.length : devuelve la cantidad de argumentos
+
+        int suma = 0; // inicio una variable en 0 para sumar los argumentos
+
+        for (String argumento : args) { // for-each     for(Tipo Variable : coleccion)
+            int numero = Integer.parseInt(argumento); // creo variable numero y convierto la cadena en int
+            suma += numero; // sumamos y almacenamos cada argumento
+        }
+
+        System.out.println("La suma de los números es: " + suma);
     }
 }
+
+
+
+
