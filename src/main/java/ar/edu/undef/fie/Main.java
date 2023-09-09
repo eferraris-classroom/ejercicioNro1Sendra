0902c6acd849d6c73e70ea287f054ae3fb0ac0be
“julioleonel” <jsendra@fie.undef.edu.ar>
2023-09-09 17:13:30 -0300

    Agrego enunciado

--- src/main/java/ar/edu/undef/fie/Main.java
+++ src/main/java/ar/edu/undef/fie/Main.java
@@ -1,5 +1,7 @@
 package ar.edu.undef.fie;
 
+//  Generar un programa que sume los numeros enteros pasados por linea de comando y de el resultado a traves de la consola
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
